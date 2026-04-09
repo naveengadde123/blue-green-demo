d1f6e53ccdf86c6aa83edd72d673a66290cc3b85
Naveen Gadde <naveen.gadde@vkraftsoftware.com>
2026-04-09 14:41:22 +0530

    modified app.py file

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-import os
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    version = os.getenv("VERSION", "UNKNOWN")
-    return f"Hello from {version} deployment!"
+    return "Hello from Blue-Green Deployment!"
 
 @app.route('/health')
 def health():
     return "OK", 200
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
